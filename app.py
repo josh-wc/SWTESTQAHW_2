9526cb077a0f90929c41be41656caa2b7aff628f
josh-wc <joshuachumneyeti@gmail.com>
2024-04-07 20:52:09 -0500

    Deleted old BMI code

--- app.py
+++ app.py
@@ -14,6 +14,21 @@ def calculate():
     bmi_value, bmi_category = bmi_calc(height_feet, height_inches, weight)
     return render_template('result.html', bmi_value=round(bmi_value, 2), bmi_category=bmi_category)
 
+# Function to input height in feet
+def height_feet_input():
+    height_feet = float(input('Enter your height in feet.\n'))
+    return height_feet
+
+# Function to input height in inches
+def height_inches_input():
+    height_inches = float(input('Enter your height in inches.\n'))
+    return height_inches
+
+# Function to input weight
+def weight_input():
+    weight = float(input('Enter your weight.\n'))
+    return weight
+
 def bmi_calc(height_feet, height_inches, weight):
     bmi = (weight * 0.45) / ((((height_feet * 12) + height_inches) * 0.025) * (((height_feet * 12) + height_inches) * 0.025))
     if round(bmi, 1) < 18.5:
